c92bd984af99908a78f5be94e02942424a3bb6dc
Francis Bongiovanni <fbongiovanni29@gmail.com>
2015-11-08 23:50:34 -0500

    Broke remote

---
+++ README.md
@@ -0,0 +1,14 @@
+This is an example tutorial for how the Vimtorial repo, built by Scripting Vim with Ruby, works. Inside of this tutorial we will learn how the bootstrap grid system works by building a colorful layout that shows how each size responds at a width of 4 out of 12. Feel free to refactor the code to learn more.
+
+
+To work the Vimtorial just type (while in command mode):
+
+:ruby load 'next.rb'
+
+and hit return. (You may have to hit it twice)
+
+If you are in the right window you will have to hit return once after loading next.rb
+
+This should be the only command you'll need to remember and if you get lost just type :q twice
+
+If you get an error run the command again you were most likely in the wrong window.
---
+++ next.rb
@@ -0,0 +1,32 @@
+#Code documented by comments in official file at https://github.com/fbongiovanni29/vimtorials
+#Fourth and Fifth file added here and in tut variable
+file = [Dir["one.*"][0], Dir["two.*"][0], Dir["three.*"][0], Dir["four.*"][0], Dir["five.*"][0]]
+page = VIM::Buffer.current
+page = page.name.split('/').last
+if page == "README.md"	
+	VIM.command("vi tut_one.md")
+	VIM.command("vs")
+	VIM.command("vertical resize 75")
+	VIM.command("vi #{Dir["one.*"][0]}")
+	VIM.command("wincmd h")
+end
+
+if VIM::Window.current == VIM::Window[1] 
+	VIM.command("wincmd h")
+	puts "Run command again incorrect window!"
+end
+
+elem = file.index(page)
+tut = ['tut_one.md', 'tut_two.md', 'tut_three.md', 'tut_four.md', 'tut_five.md']
+
+#Modulos changed to expand to five files
+VIM.command("vertical resize 75")
+VIM.command("vi #{file[(elem+1)%5]}")
+VIM.command("wincmd l")
+VIM.command("vi #{tut[(elem+1)%5]}")
+VIM.command("wincmd h")
+
+if file[elem].split('.')[1].to_s == "open"
+	`open #{file[elem]}`
+end
+
